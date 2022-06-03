cfa72f85d308fb0080145b9ac63f305df9337297
Ilim-Hussein <ilim11.net@gmail.com>
2022-06-03 11:50:03 +0600

    Add icon

--- CounterRakaat_V2.Android/MainActivity.cs
+++ CounterRakaat_V2.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.Gms.Ads;
 
 namespace CounterRakaat_V2.Droid
 {
-    [Activity(Label = "CounterRakaat_V2", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "CounterRakaat_V2", Icon = "@mipmap/icon_namaz", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         public static Activity ActivityCurrent { get; private set; }
